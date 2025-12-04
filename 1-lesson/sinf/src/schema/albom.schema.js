827fc5bebfa33f706db8362d025088e4fc21424e
Anorqulov14 <eldoranorqulov@gmail.com>
2025-12-04 20:06:55 +0500

    uyga

--- 1-lesson/sinf/src/schema/albom.schema.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const albomSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  artist: String,
-  createdAt: { type: Date, default: Date.now }
-});
-
-export default mongoose.model("Albom", albomSchema);
